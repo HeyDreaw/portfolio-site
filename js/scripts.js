998d2547ed0fd651eb19eb6c01770f6590e62f52
HeyDreaw <asuppinimusic@gmail.com>
2016-06-15 16:28:48 -0500

    Adjusted fade in for JS, modified Nav for mobile, made Aguirre clickable for mobile

--- js/scripts.js
+++ js/scripts.js
@@ -29,11 +29,11 @@ $(document).ready(function(){
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             
            
-            bottom_of_window = bottom_of_window + 10;  
+            bottom_of_window = bottom_of_window + 300;  
           
             if( bottom_of_window > bottom_of_object ){
                 
-                $(this).animate({'opacity':'1'},400);
+                $(this).animate({'opacity':'1'},600);
                     
             }
         }); 
