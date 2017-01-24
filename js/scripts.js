37b1635131fc13f1350473b788899ebc341b2828
HeyDreaw <asuppinimusic@gmail.com>
2017-01-23 21:13:42 -0800

    Implementing site redesign

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,13 @@
 $(document).ready(function(){
-	
-	
-	$(window).scroll(function(){                          
-		if ($(this).scrollTop() > 400) {
-				$('.menu').fadeIn(500);
-		} else {
-				$('.menu').fadeOut(500);
-		}
-	});
-	
-	$(function() {
-		count = 0;
-		myTitles = ["coffee addict", "comic reader", "guitar player", "total nerd", "web developer"];
-		setInterval(function () {
-			count++;
-			$("#my-title").fadeOut(400, function () {
-				$(this).text(myTitles[count % myTitles.length]).fadeIn(400);
-			});
-		}, 2000);
-	});
-	
-	
-	setTimeout(function (){
-		$('#main-logo').animate({
-			opacity: 1
-		}, 1500);
-	});
 
+$('.report-card.treehouse').reportCard({
+    userName: 'heydreaw',
+    site: 'treehouse',
+    badgesAmount: 7
+});
+
+	
+	
 	
 	
 });
